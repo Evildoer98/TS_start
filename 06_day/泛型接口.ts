5cff951d543c022b4b8c26de60c2862e937f480d
徐世成 <1213089093@qq.com>
2021-10-06 22:48:06 +0800

    泛型接口

--- 06_day/泛型接口.ts
+++ 06_day/泛型接口.ts
@@ -1,4 +1,35 @@
+// 函数类型接口
+/**
+ * interface ConfigFn {
+ *      (value1: string, value2: string): string;
+ * }
+ * 
+ * var setData:ConfigFn = function(value1: string, value2: string):string {
+ *      return value1 + value2
+ * }
+ * 
+ * setData('name','张三')
+ */
 
-interface ConfigFn {
-    (value1: string, value2: string): string;
+// 
+
+
+// 泛型接口
+interface ConfigFn1 {
+    <T>(value: T): T
 }
+
+var getData1:ConfigFn1 = function <T>(value: T): T {
+    return value
+}
+
+getData1<string>('张三');
+
+interface ConfigFn2<T> {
+    (value: T): T
+}
+function getData2<T>(value: T): T {
+    return value
+}
+var myGetData: ConfigFn2<string> = getData;
+myGetData('Evildoer')
