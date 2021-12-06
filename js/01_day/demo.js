eeba5a2054d1a9dc3e1f15c6dc232bad89329cc2
徐世成 <1213089093@qq.com>
2021-12-06 21:30:08 +0800

    chore: modify pr template

--- js/01_day/demo.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ts 编译成 es5
-var str = 'hello';
-console.log(str);
-console.log('hello');
--- js/05_day/可索引接口.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// 可索引接口: 数组、对象的约束 （不常用）
-// ts 定义数组的方式
-var arr = [123, 234];
-var arr6 = ['123', '234'];
-var arr7 = ['123', '234'];
-console.log(arr[0]); // 123
-var arr8 = { name: '张三' };
