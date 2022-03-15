6297339f312e8fd83ee96f58831f3250a0e4d586
徐世成 <1213089093@qq.com>
2022-03-15 19:04:56 +0800

    feat(everyday-types): add array and any types

---
+++ everyday-types/arrays.ts
@@ -0,0 +1,18 @@
+// eg:[1, 2, 3]
+const array1: Array<number> = [1, 2, 3]
+const array2: number[] = [1, 2, 3]
+
+
+// eg:['a', 'b', 'c']
+const array3: Array<string> = ['a', 'b', 'c']
+const array4: string[] = ['a', 'b', 'c']
+
+/**
+ *
+ * 两种方法均可
+ * 泛型 T<U>
+ *
+ * ps: 其中 [number] 为元祖类型
+ * https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+ *
+ */
