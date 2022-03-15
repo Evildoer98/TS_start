bd901376d7b431ec70c86d2b02133c82a2233115
徐世成 <1213089093@qq.com>
2022-03-15 18:42:25 +0800

    feat(everyday-types): add the-primitives

---
+++ everyday-types/the-primitives.ts
@@ -0,0 +1,15 @@
+// The primitives: string, number, and boolean
+/**
+ *
+ * js 中原始类型 string、number、boolean
+ *
+ * 类型名称 String、Number 和 Boolean（以大写字母开头）是合法的，
+ * 但指的是一些很少出现在代码中的特殊内置类型。
+ * 始终对类型使用字符串、数字或布尔值。
+ *
+ */
+
+const string: String = 'hello'
+const number: Number = 18
+const boolean: Boolean = true
+
